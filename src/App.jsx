ca763bc91cdd63afe416fe1f9b15a33a34150214
Neranajan Dissanayake <62052474+MVNDissanayake@users.noreply.github.com>
2026-01-12 22:31:48 +0530

    bug fixiing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "@/pages/Home";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
-import { Toaster } from "@/components/ui/Toaster";
-
 
 function App() {
   return (
-    <>
-    <Toaster />
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/k1live-web/",
+  base: "/k1live-web",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
